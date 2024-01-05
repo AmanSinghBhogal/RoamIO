c18780af16a881e5671384e713359aff901fb36c
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-01-05 16:55:02 +0530

    Home Page Completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,30 @@
+import styles from './home.module.css';
+import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+
+        <h1 className={styles.title}>
+          Creative Thoughts Agency.
+        </h1>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio adipisci voluptatibus pariatur quia. Cupiditate magni porro laborum harum hic deserunt eum voluptas beatae pariatur vitae? Magni, sed odio numquam error consequatur quidem recusandae voluptatum ea. Incidunt dolor eos libero velit.</p>
+
+        <div className={styles.buttons}>
+          <button className={styles.btn}>Learn More</button>
+          <button className={styles.btn}>Contact</button>
+        </div>
+
+        <div className={styles.brands}>
+          <Image src="/assets/images/brands.png" alt='Brands' fill className={styles.brandsImg}  />
+        </div>
+
+      </div>
+      <div className={styles.imgContainer}>
+          <Image src="/assets/images/hero.gif" alt='Home GIF' fill className={styles.homeGif} />
+      </div>
+    </div>
   )
 }
 
