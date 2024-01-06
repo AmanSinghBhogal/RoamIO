db6a01c91951f2c202800f9a395f3e08da7516a6
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-01-06 17:09:19 +0530

    Contact Page UI Completed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,27 @@
+"use client";
+import styles from './contact.module.css';
+import Image from 'next/image';
 
 const Contact = () => {
+
+  const handleSubmit = (e) => {
+  }
+
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+          <Image src="/assets/images/contact.png" alt="Contact Image" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action={handleSubmit} className={styles.form}>
+          <input type="text" placeholder='Full Name*' required />
+          <input type="email" placeholder='Email Address*' required />
+          <input type="number" placeholder='Mobile Number (Optional)' />
+          <textarea name="" id="" cols="30" rows="10" placeholder='Message*' required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
