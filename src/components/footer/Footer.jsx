583bb9c3ed92872216f3ee46ae5b296a45b57b27
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-01-06 15:51:34 +0530

    Footer Completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className={styles.container}>
+        <div className={styles.org}>
+          <span className={styles.logo}>RoamIO</span>
+          <span className={styles.year}>&copy;{(new Date().getFullYear())}</span>
+        </div>
+        <div className={styles.text}>Unleash Your Wanderlust, One Journey at a Time!</div>
     </div>
   )
 }
