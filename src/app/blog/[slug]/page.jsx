06e35c26f188560b48b928959ffbd93a9c857c6e
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-01-15 12:43:26 +0530

    Updated Single Post Page UI

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -23,18 +23,15 @@ const SingleBlog = async ({params}) => {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src="/assets/images/post.jpg" alt='Post Img' width={400} height={400} className={styles.img}/>
+        <Image src={post.img && post.img !== 'none'? post.img : "/assets/images/post.jpg" } alt='Post Img' width={400} height={400} className={styles.img}/>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.title}>{post.title}</div>
         <div className={styles.detail}>
-          <div className={styles.detailAvtar}>
-            <Image src="/assets/images/post.jpg" alt='Post Img' width={50} height={50} className={styles.avatar} />
-          </div>
           <PostUser id={post.userId} /> 
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>01.01.2024</span>
+            <span className={styles.detailValue}>{post.createdAt.toString().slice(4,15)}</span>
           </div>
         </div>
         <div className={styles.content}>
--- src/components/PostUser/PostUser.jsx
+++ src/components/PostUser/PostUser.jsx
@@ -1,6 +1,7 @@
 import styles from './PostUser.module.css';
 import { getUser } from "@/lib/data";
 import { Suspense } from "react";
+import Image from 'next/image';
 
 const PostUser = async ({id}) => {
     const user = await getUser(id);
@@ -8,8 +9,13 @@ const PostUser = async ({id}) => {
     return (
       <Suspense fallback={<div>Loading...</div>}>
           <div className={styles.container}>
-              <span className={styles.detailTitle}>Author</span>
+            <div className={styles.detailAvtar}>
+                <Image src={user.img? user.img: "/assets/images/noavatar.png"} alt='Post Img' width={50} height={50} className={styles.avatar} />
+            </div>
+            <div className={styles.detailAuthor}>
+                <span className={styles.detailTitle}>Author</span>
               <span className={styles.detailValue}>{user.username}</span>
+            </div>
           </div>
       </Suspense>
     )
