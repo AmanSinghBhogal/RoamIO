cd6d647d96f043e676cebb7345bbf45160093763
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-01-15 13:04:13 +0530

    Blogs page UI updated

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -6,10 +6,10 @@ const PostCard = ({post}) => {
   return (
     <div className={styles.container}>
         <div className={styles.top}>
-          <div className={styles.ImgContainer}>
-            <Image src="/assets/images/post.jpg" alt='Post Image' fill className={styles.img} />
-          </div> 
-          <span className={styles.date}>01.01.2024</span>
+          <div>
+            <Image src={post.img && post.img !== 'none'? post.img : "/assets/images/post.jpg" } alt='Post Image' height={300} width={300} className={styles.img} />
+          </div>
+          <div className={styles.date}>{post.createdAt.toString().slice(4,15)}</div>
         </div>
         <div className={styles.bottom}>
           <h1 className={styles.title}>{post.title}</h1>
